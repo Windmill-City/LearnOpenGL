dd9539071c99f7c9534f1b6c70179b230f945e13
Windmill_City <1449182174@qq.com>
2023-02-08 15:26:09 +0800

    colorful tri

--- src/TriRenderer.cpp
+++ src/TriRenderer.cpp
@@ -6,11 +6,10 @@
 
 float vertices[] = {
     // clang-format off
-    0.5f, 0.5f, 0.0f,   // 右上角
-    0.5f, -0.5f, 0.0f,  // 右下角
-    -0.5f, -0.5f, 0.0f, // 左下角
-    -0.5f, 0.5f, 0.0f   // 左上角
-    -0.0f, 0.0f, 0.0f   // 中间
+    // 位置              // 颜色
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
     // clang-format on
 };
 
@@ -20,8 +19,7 @@ unsigned int indices[] = {
     // 这样可以由下标代表顶点组合成矩形
 
     // clang-format off
-    0, 1, 4, // 第一个三角形
-    2, 3, 4  // 第二个三角形
+    0, 1, 2, // 第一个三角形
     // clang-format on
 };
 
@@ -120,8 +118,12 @@ TriRenderer::TriRenderer()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Vertex Attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // 位置属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // 颜色属性
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // Wireframe
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
