03a889d246630a3ac98fc597b7c5d4c680dbb558
Windmill_City <1449182174@qq.com>
2023-08-16 15:00:26 +0800

    cleanup

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -6,10 +6,11 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <stdint.h>
 
 #include "Resource.hpp"
 
-class Shader
+struct Shader
 {
   protected:
     unsigned int ID;
@@ -32,22 +33,19 @@ class Shader
      * @param name uniform name
      * @param value bool value
      */
-    void setBool(const std::string& name, bool value) const;
+    void setBool(const std::string_view name, bool value) const;
     /**
      * @brief Set uniform int value
      *
      * @param name uniform name
      * @param value int value
      */
-    void setInt(const std::string& name, int value) const;
+    void setInt(const std::string_view name, int value) const;
     /**
      * @brief Set uniform float value
      *
      * @param name uniform name
      * @param value float value
      */
-    void setFloat(const std::string& name, float value) const;
-
-  protected:
-    void checkCompileErrors(unsigned int shader, std::string type);
+    void setFloat(const std::string_view name, float value) const;
 };
