199c056cb3bf8afb4be45f99cd14895d0e8c1989
Windmill_City <1449182174@qq.com>
2023-08-16 17:10:10 +0800

    add codecvt test

--- src/CodeCvt.cpp
+++ src/CodeCvt.cpp
@@ -1,5 +1,4 @@
 #include "CodeCvt.hpp"
-#include "Exception.hpp"
 
 std::u16string CodeCvt::to_u16string(const std::wstring_view u16) noexcept
 {
@@ -15,29 +14,26 @@ std::u16string CodeCvt::to_u16string(const std::string_view u8) noexcept
     for (char ch8 : u8)
     {
         // UTF-8 CodePoint Masks
-        char cp_7_m  = 0b10000000;
-        char cp_11_m = 0b11100000;
-        char cp_16_m = 0b11110000;
-        char cp_21_m = 0b11111000;
-        char cp_26_m = 0b11111100;
-        char cp_31_m = 0b11111110;
+        char cp_7_m  = (char)0b10000000;
+        char cp_11_m = (char)0b11100000;
+        char cp_16_m = (char)0b11110000;
+        char cp_21_m = (char)0b11111000;
+        char cp_26_m = (char)0b11111100;
+        char cp_31_m = (char)0b11111110;
+        // UTF-8 CodePoints(Byte1)
+        char cp_7    = (char)0b00000000;
+        char cp_11   = (char)0b11000000;
+        char cp_16   = (char)0b11100000;
+        char cp_21   = (char)0b11110000;
+        char cp_26   = (char)0b11111000;
+        char cp_31   = (char)0b11111100;
+        // Extra Codepoint(Byte2~6)
+        char cp_v_m  = (char)0b11000000;
+        char cp_v    = (char)0b10000000;
 
-        // UTF-8 CodePoints Matcher
-        char cp_7  = 0b00000000;
-        char cp_11 = 0b11000000;
-        char cp_16 = 0b11100000;
-        char cp_21 = 0b11110000;
-        char cp_26 = 0b11111000;
-        char cp_31 = 0b11111100;
-
-        // Codepoint Value Mask
-        char cp_v_m = 0b11000000;
-        // Codepoint Value Matcher
-        char cp_v   = 0b10000000;
-
-        if (!cp_s && ch8 & cp_v_m == cp_v) continue;
+        if (!cp_s && (ch8 & cp_v_m) == cp_v) continue;
         // Process extra codepoints - 0b10xxxxxx
-        if (cp_s && ch8 & cp_v_m == cp_v)
+        if (cp_s && (ch8 & cp_v_m) == cp_v)
         {
             cp_s -= 6;
             ch16 |= ((char16_t)ch8 & 0b00111111) << cp_s;
@@ -46,7 +42,7 @@ std::u16string CodeCvt::to_u16string(const std::string_view u8) noexcept
         }
 
         // Codepoint 7
-        if (ch8 & cp_7_m == cp_7)
+        if ((ch8 & cp_7_m) == cp_7)
         {
             cp_s = 0;
             ch16 = ch8 & 0b01111111;
@@ -55,7 +51,7 @@ std::u16string CodeCvt::to_u16string(const std::string_view u8) noexcept
         }
 
         // Codepoint 11
-        if (ch8 & cp_11_m == cp_11)
+        if ((ch8 & cp_11_m) == cp_11)
         {
             cp_s = 11 - 5;
             ch16 = ch8 << cp_s;
@@ -63,7 +59,7 @@ std::u16string CodeCvt::to_u16string(const std::string_view u8) noexcept
         }
 
         // Codepoint 16
-        if (ch8 & cp_16_m == cp_16)
+        if ((ch8 & cp_16_m) == cp_16)
         {
             cp_s = 16 - 4;
             ch16 = ch8 << cp_s;
@@ -94,13 +90,13 @@ std::string CodeCvt::to_string(const std::u16string_view u16) noexcept
     for (char16_t ch16 : u16)
     {
         // Codepoint 7
-        if (ch16 <= 0x00 && ch16 >= 0x7F)
+        if (ch16 >= 0x00 && ch16 <= 0x7F)
         {
             ss << (char)ch16;
         }
 
         // Codepoint 11
-        if (ch16 <= 0x80 && ch16 >= 0x7FF)
+        if (ch16 >= 0x80 && ch16 <= 0x7FF)
         {
             char ch8_1 = 0b11000000 | ch16 >> 6;
             char ch8_2 = 0b10000000 | (ch16 & 0b00111111);
@@ -108,7 +104,7 @@ std::string CodeCvt::to_string(const std::u16string_view u16) noexcept
         }
 
         // Codepoint 16
-        if (ch16 <= 0x80 && ch16 >= 0x7FF)
+        if (ch16 >= 0x800 && ch16 <= 0xFFFF)
         {
             char ch8_1 = 0b11100000 | ch16 >> 12;
             char ch8_2 = 0b10000000 | (ch16 >> 6 & 0b00111111);
